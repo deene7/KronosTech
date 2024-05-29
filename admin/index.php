c943c270002f1adfd533580e92642aad73c563df
Paulo <160801507+paulinr04@users.noreply.github.com>
2024-05-29 15:49:15 -0300

    admin

---
+++ admin/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel de Controle | KronosTech</title>
+    <link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" >
+
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <!--BARRRA LATERAL -->
+    <div class="sidebar">
+        <div class="sidebar-brand">
+            <h2><span class="lab la-accusoft"></span> KronosTech</h2>
+        </div>
+
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <a href="" class="active"><span class="las la-igloo"></span>
+                    <span>Painel de Controle</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-receipt"></span>
+                    <span>Pedidos</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-clipboard-list"></span>
+                    <span>Produtos</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-users"></span>
+                    <span>Usuários</span></a>
+                </li>
+                <li>
+                    <a href=""><span class="las la-user-circle"></span>
+                    <span>Conta</span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!--CONTEUDO -->
+    <div class="main-content">
+        <header>
+                <h2>
+                    <label for="">
+                        <span class="las la-bars"></span>
+                    </label>
+
+                    Painel de Controle
+                </h2>
+
+
+                <div class="user-wrapper">
+                    <img src="img2.jpg" width="40px" height="40px">
+                    <div>
+                        <h4>Admin</h4>
+                        <small>Adm. KronosTech</small>
+                    </div>
+                </div>
+        </header>  
+        
+        <main>
+            <div class="cards">
+                <div class="card-single">
+                    <div>
+                        <h1>54</h1>
+                        <span>Clientes</span>
+                    </div>
+                    <div>
+                        <span class="las la-users"></span>
+                    </div>
+                </div>
+                <div class="card-single">
+                    <div>
+                        <h1>79</h1>
+                        <span>Projetos</span>
+                    </div>
+                    <div>
+                        <span class="las la-clipboard"></span>
+                    </div>
+                </div>
+                <div class="card-single">
+                    <div>
+                        <h1>100</h1>
+                        <span>Pedidos</span>
+                    </div>
+                    <div>
+                        <span class="las la-shopping-bag"></span>
+                    </div>
+                </div>
+                <div class="card-single">
+                    <div>
+                        <h1>R$5.000,00</h1>
+                        <span>Lucro</span>
+                    </div>
+                    <div>
+                        <span class="las la-google-wallet"></span>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+    
+</body>
+</html>

0733a5221788ee98b639a1fa2433f6383e8092d5
Paulo <160801507+paulinr04@users.noreply.github.com>
2024-06-06 02:19:34 -0300

    funcionando 100%

--- admin/add_product.php
+++ admin/add_product.php
@@ -1,5 +1,19 @@
 <?php include('header.php') ?>
 
+<?php 
+include('../server/connection.php');
+
+// Consulta para obter categorias distintas
+$query = "SELECT DISTINCT product_category FROM products";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    $categories = $result->fetch_all(MYSQLI_ASSOC);
+} else {
+    $categories = [];
+}
+?>
+
 <div class="recent-grid">
     <div class="projects">
         <div class="card">
@@ -13,13 +27,19 @@
                     <h4>Nome:</h4> <input type="text" name="name" class="input-large" placeholder="Nome do Produto">
                     <h4>Descrição:</h4> <textarea name="description" class="input-large-textarea" placeholder="Descrição do Produto"></textarea>
                     <h4>Valor (R$):</h4> <input type="text" id="preco" name="price" class="input-large" placeholder="Valor do Produto" maxlength="7">
-                    <h4>Categoria:</h4> <input type="text" name="category" class="input-large" placeholder="Categoria do Produto">
-                    <h4>Imagem 1:</h4> <input type="file">
-                    <h4>Imagem 2:</h4> <input type="file">
-                    <h4>Imagem 3:</h4> <input type="file">
-                    <h4>Imagem 4:</h4> <input type="file"> <br> <br>
+                    <h4>Categoria:</h4> 
+                    <select name="category" class="input-large">
+                        <option value="" disabled selected>Selecione a categoria correspondente</option>
+                        <?php foreach ($categories as $category): ?>
+                            <option class="category-option" value="<?php echo htmlspecialchars($category['product_category']); ?>"><?php echo htmlspecialchars($category['product_category']); ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                    <h4>Imagem 1:</h4> <input type="file" id="image1" name="image1">
+                    <h4>Imagem 2:</h4> <input type="file" id="image2" name="image2">
+                    <h4>Imagem 3:</h4> <input type="file" id="image3" name="image3">
+                    <h4>Imagem 4:</h4> <input type="file" id="image4" name="image4"> <br> <br>
                     
-                    <input type="submit" class="btn btn-primary" name="edit_btn" value="Adicionar">
+                    <input type="submit" class="btn btn-primary" name="create_product" value="Adicionar">
                 </form>
             </div>
         </div>

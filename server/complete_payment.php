ffc6e92e6f8a16df37b9d13d483ef5136ebdffd4
Paulo <160801507+paulinr04@users.noreply.github.com>
2024-06-11 20:49:39 -0300

    Add files via upload

--- server/complete_payment.php
+++ server/complete_payment.php
@@ -1,12 +1,16 @@
 <?php
 session_start();
 include('connection.php');
+date_default_timezone_set('America/Sao_Paulo'); 
 
 if (isset($_GET['transaction_id']) && isset($_GET['order_id'])) {
     $order_id = $_GET['order_id'];
     $order_status = "Pedido Pago";
     $transaction_id = $_GET['transaction_id'];
     $user_id = $_SESSION['user_id'];
+    $payment_date = date('Y-m-d H:i:s');
+
+    // Definindo o fuso horário para São Paulo
 
     // Mudar order_status para PEDIDO PAGO
     $stmt = $conn->prepare("UPDATE orders SET order_status=? WHERE order_id=?");
@@ -15,8 +19,8 @@ if (isset($_GET['transaction_id']) && isset($_GET['order_id'])) {
     $stmt->execute();
 
     // Salvar info do pagamento no banco
-    $stmt1 = $conn->prepare("INSERT INTO payments (order_id, user_id, transaction_id) VALUES (?, ?, ?);");
-    $stmt1->bind_param('iis', $order_id, $user_id, $transaction_id);
+    $stmt1 = $conn->prepare("INSERT INTO payments (order_id, user_id, transaction_id, payment_date) VALUES (?, ?, ?, ?);");
+    $stmt1->bind_param('iiss', $order_id, $user_id, $transaction_id, $payment_date);
 
     $stmt1->execute();
 
